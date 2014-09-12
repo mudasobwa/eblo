4e5c8bab24da32f91177cb66fded3d5dcce04885
Alexei Matyushkin <am@mudasobwa.ru>
2014-09-12 08:55:45 +0200

    Init script modifications.

--- init_repo.sh
+++ init_repo.sh
@@ -3,8 +3,14 @@
 which bower || exit 1
 which composer || exit 2
 
+echo 'Initializing submodules...'
+git submodule init
+
+echo 'Bootstrapping node, bower and composer...'
 npm install
 bower install
 cd app
 composer install
 cd -
+
+echo 'Done.'
