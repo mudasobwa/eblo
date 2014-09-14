cc0ea80cfa75f3f4af12db2a9b27d3bab43e630e
Alexei Matyushkin <am@mudasobwa.ru>
2014-09-14 10:00:32 +0200

    History is almost working (some side effects on 1st page.)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,6 @@
 (function(document) {
-  'use strict';
-
-  document.addEventListener('polymer-ready', function() {
-    // Perform some behaviour
-    console.log('Polymer is ready to rock!');
-  });
-
-// wrap document so it plays nice with other libraries
-// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
+	'use strict';
+	document.addEventListener('polymer-ready', function() {
+		console.log('%cPolymer is ready to rock!', 'color:green;');
+	});
 })(wrap(document));
